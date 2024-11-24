7726732657669078eb90509a0f8d15a1d9a48a6b
Giulio <giulio.appetito.ga@gmail.com>
2024-11-25 00:13:09 +0100

    Added Problems and experiment file. Problems is still empty.

--- Flink-cep-examples-main/src/main/java/module-info.java
+++ Flink-cep-examples-main/src/main/java/module-info.java
@@ -4,5 +4,7 @@ module flinkCEP.Patterns {
     requires org.apache.commons.csv;
     requires flink.core;
     requires flink.streaming.java;
+    opens problem to io.github.ericmedvet.jnb.core;
+
 }
 
